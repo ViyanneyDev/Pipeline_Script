78c647004d6ef06bb5cc7a93dacdd84e3f633671
ViyanneyDev <viyanneym@crownims.com>
2025-11-07 17:29:09 +0530

    Enhance Build.bat to install npm dependencies

--- Build.bat
+++ Build.bat
@@ -1,1 +1,10 @@
-echo "Building the Project : %date% : %time%"
+@echo off
+if exist package.json (
+  echo Installing npm dependencies...
+  call npm install
+
+  echo Running build...
+  call npm run build
+) else (
+  echo No package.json found - skipping npm build
+)
